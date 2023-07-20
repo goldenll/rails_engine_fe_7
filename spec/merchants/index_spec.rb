dcce549fc8bc52dd71c1615c926a9871f0b703ca
Lauren Golden <llgolden@gmail.com>
2023-07-20 11:30:28 -0600

    Refactored route names

--- spec/merchants/index_spec.rb
+++ spec/merchants/index_spec.rb
@@ -1,15 +1,14 @@
 require "rails_helper"
 
-RSpec.describe "merchants index page" do 
+RSpec.describe "merchants index page", type: :feature do 
   it "displays a list of merchant names that link to their show page"  do 
-    # visit merchants_path ; I'm not sure why this is not working 
-    visit "/merchants"
+    visit merchants_path
 
     expect(page).to have_link("Schroeder-Jerde")
     expect(page).to have_link("Koepp LLC")
     expect(page).to have_link("Glover Inc")
 
     click_link "Schroeder-Jerde"
-    expect(current_path).to eq("/merchants/1")
+    expect(current_path).to eq(merchant_path(1))
   end
 end
--- spec/merchants/show_spec.rb
+++ spec/merchants/show_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
-RSpec.describe "merchant show page" do 
+RSpec.describe "merchant show page", type: :feature do 
   it "displays a list of items the merchant sells"  do 
-    visit "/merchants/1"
+    visit merchant_path(1)
 
     expect(page).to have_content("Item Nemo Facere")
     expect(page).to have_content("Item Provident At")
